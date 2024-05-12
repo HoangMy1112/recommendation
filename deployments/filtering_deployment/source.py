bbf5a739e0e5de8917d7ac4df075ce63ffddc002
HoangMy1112 <nhm8606@gmail.com>
2024-05-12 21:39:29 +0700

    add

--- deployments/filtering_deployment/source.py
+++ deployments/filtering_deployment/source.py
@@ -4,10 +4,11 @@ from app.models.collaborative_filtering import MatrixFactorization
 import modelbit
 import os
 
-os.environ["AWS_ACCESS_KEY_ID"] = "AKIA5HPKATSKPD7NFGAS"
-os.environ["AWS_SECRET_ACESS_KEY"] = "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"
 
 def recommend():
+    os.environ["AWS_ACCESS_KEY_ID"] = "AKIA5HPKATSKPD7NFGAS"
+    os.environ["AWS_SECRET_ACESS_KEY"] = "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"
+    
     combined_data = []
     all_recommendations = {}
     try:
